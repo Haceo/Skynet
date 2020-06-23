92756d906716eb810a7e72026bf71446057efacb
Thomas Aubin <shetah1@yahoo.com>
2020-06-23 04:59:17 -0400

    fixed streamer editor load

--- Skynet/Modules/Misc.cs
+++ Skynet/Modules/Misc.cs
@@ -1,16 +1,10 @@
 ﻿using Discord.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using TwitchLib.Api;
 
 namespace Skynet.Modules
 {
     public class Misc : ModuleBase<SocketCommandContext>
     {
-        public TwitchAPI _api = new TwitchAPI();
         [Command("emoteid", RunMode = RunMode.Async)]
         public async Task GetEmoteId(string emote)
         {
